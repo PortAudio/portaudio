c5874f23c465bc8df6252f0c4db19a7852d15bba
robiwan <robiwan@0f58301d-fd10-0410-b4af-bbb618454e57>
2012-02-14 09:32:57 +0000

    Merged WaveRT branch (excluding stuff pertaining to ticket #169)

--- src/common/pa_trace.h
+++ src/common/pa_trace.h
@@ -84,12 +84,28 @@ extern "C"
 void PaUtil_ResetTraceMessages();
 void PaUtil_AddTraceMessage( const char *msg, int data );
 void PaUtil_DumpTraceMessages();
-    
+
+/* Alternative interface */
+
+typedef void* LogHandle;
+
+int PaUtil_InitializeHighSpeedLog(LogHandle* phLog, unsigned maxSizeInBytes);
+void PaUtil_ResetHighSpeedLogTimeRef(LogHandle hLog);
+int PaUtil_AddHighSpeedLogMessage(LogHandle hLog, const char* fmt, ...);
+void PaUtil_DumpHighSpeedLog(LogHandle hLog, const char* fileName);
+void PaUtil_DiscardHighSpeedLog(LogHandle hLog);
+
 #else
 
 #define PaUtil_ResetTraceMessages() /* noop */
 #define PaUtil_AddTraceMessage(msg,data) /* noop */
 #define PaUtil_DumpTraceMessages() /* noop */
+
+#define PaUtil_InitializeHighSpeedLog(phLog, maxSizeInBytes)  (0)
+#define PaUtil_ResetHighSpeedLogTimeRef(hLog)
+#define PaUtil_AddHighSpeedLogMessage(...)   (0)
+#define PaUtil_DumpHighSpeedLog(hLog, fileName)
+#define PaUtil_DiscardHighSpeedLog(hLog)
 
 #endif
 
