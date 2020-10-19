8118be4c46c153b723a76c676bd22d8620b428c9
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2020-10-19 15:54:21 +0300

    PulseAudio: Update copyright year 2016 to 2020

--- src/hostapi/pulseaudio/pa_linux_pulseaudio.c
+++ src/hostapi/pulseaudio/pa_linux_pulseaudio.c
@@ -3,7 +3,7 @@
  * PulseAudio host to play natively in Linux based systems without
  * ALSA emulation
  *
- * Copyright (c) 2014-2016 Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
+ * Copyright (c) 2014-2020 Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
  * Copyright (c) 2016 Sqweek <sqweek@gmail.com>
  *
  * Based on the Open Source API proposed by Ross Bencina
