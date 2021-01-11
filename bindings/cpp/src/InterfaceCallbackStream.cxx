3081f302e777ca001e93bd031a16128088865f62
Be <be@mixxx.org>
2021-01-11 15:25:00 -0600

    C++ bindings: reorganize files

---
+++ bindings/cpp/src/InterfaceCallbackStream.cxx
@@ -0,0 +1,45 @@
+#include "InterfaceCallbackStream.hxx"
+
+#include "StreamParameters.hxx"
+#include "Exception.hxx"
+#include "CallbackInterface.hxx"
+
+namespace portaudio
+{
+
+    // ---------------------------------------------------------------------------------==
+
+    InterfaceCallbackStream::InterfaceCallbackStream()
+    {
+    }
+
+    InterfaceCallbackStream::InterfaceCallbackStream(const StreamParameters &parameters, CallbackInterface &instance)
+    {
+        open(parameters, instance);
+    }
+
+    InterfaceCallbackStream::~InterfaceCallbackStream()
+    {
+        try
+        {
+            close();
+        }
+        catch (...)
+        {
+            // ignore all errors
+        }
+    }
+
+    // ---------------------------------------------------------------------------------==
+
+    void InterfaceCallbackStream::open(const StreamParameters &parameters, CallbackInterface &instance)
+    {
+        PaError err = Pa_OpenStream(&stream_, parameters.inputParameters().paStreamParameters(), parameters.outputParameters().paStreamParameters(),
+            parameters.sampleRate(), parameters.framesPerBuffer(), parameters.flags(), &impl::callbackInterfaceToPaCallbackAdapter, static_cast<void *>(&instance));
+
+        if (err != paNoError)
+        {
+            throw PaException(err);
+        }
+    }
+}
