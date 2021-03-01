bd6c02661886614b0ee0a089253dcacd5ccfdea5
EvanBalster <evan@interactopia.com>
2021-02-28 19:50:13 -0800

    Amend comments about JackRouter

--- src/hostapi/asio/pa_asio.cpp
+++ src/hostapi/asio/pa_asio.cpp
@@ -1304,9 +1304,10 @@ PaError PaAsio_Initialize( PaUtilHostApiRepresentation **hostApi, PaHostApiIndex
                 continue;
             }
 
-            // Blacklist dynamic loading of JackRouter in 32-bit applications.
+            // Blacklist dynamic loading of JackRouter ASIO driver by 64-bit applications.
             // This causes an instant crash due to a long-standing address translation error.
             //   https://github.com/jackaudio/jack2/issues/332
+            //   https://github.com/jackaudio/jack2/issues/275
 
             if (sizeof(void*)>4 && strcmp (names[i],"JackRouter") == 0)
             {
