a563f2d39d548385814f7087d65aeb9622d4e162
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2021-04-29 02:23:52 +0200

    Whitespace cleanup in Java and C++ bindings#458)

--- bindings/java/c/src/com_portaudio_PortAudio.c
+++ bindings/java/c/src/com_portaudio_PortAudio.c
@@ -26,13 +26,13 @@
  */
 
 /*
- * The text above constitutes the entire PortAudio license; however, 
+ * The text above constitutes the entire PortAudio license; however,
  * the PortAudio community also makes the following non-binding requests:
  *
  * Any person wishing to distribute modifications to the Software is
  * requested to send the modifications to the original developer so that
- * they can be incorporated into the canonical version. It is also 
- * requested that these non-binding requests be included along with the 
+ * they can be incorporated into the canonical version. It is also
+ * requested that these non-binding requests be included along with the
  * license above.
  */
 
@@ -48,7 +48,7 @@
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getVersion
   (JNIEnv *env, jclass clazz)
 {
-	return Pa_GetVersion();
+    return Pa_GetVersion();
 }
 
 /*
@@ -59,7 +59,7 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getVersion
 JNIEXPORT jstring JNICALL Java_com_portaudio_PortAudio_getVersionText
   (JNIEnv *env, jclass clazz)
 {
-	return (*env)->NewStringUTF(env, Pa_GetVersionText() );
+    return (*env)->NewStringUTF(env, Pa_GetVersionText() );
 }
 
 /*
@@ -70,8 +70,8 @@ JNIEXPORT jstring JNICALL Java_com_portaudio_PortAudio_getVersionText
 JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_initialize
   (JNIEnv *env, jclass clazz)
 {
-	PaError err = Pa_Initialize();
-	jpa_CheckError( env, err );
+    PaError err = Pa_Initialize();
+    jpa_CheckError( env, err );
 }
 
 /*
@@ -82,8 +82,8 @@ JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_initialize
 JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_terminate
   (JNIEnv *env, jclass clazz)
 {
-	PaError err = Pa_Terminate();
-	jpa_CheckError( env, err );
+    PaError err = Pa_Terminate();
+    jpa_CheckError( env, err );
 }
 
 /*
@@ -94,8 +94,8 @@ JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_terminate
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDeviceCount
   (JNIEnv *env, jclass clazz)
 {
-	jint count = Pa_GetDeviceCount();
-	return jpa_CheckError( env, count );
+    jint count = Pa_GetDeviceCount();
+    return jpa_CheckError( env, count );
 }
 
 /*
@@ -106,27 +106,27 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDeviceCount
 JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_getDeviceInfo
   (JNIEnv *env, jclass clazz, jint index, jobject deviceInfo)
 {
-	const PaDeviceInfo *info;
-	     /* Get a reference to obj's class */
-	jclass cls = (*env)->GetObjectClass(env, deviceInfo);
- 
-	info = Pa_GetDeviceInfo( index );
-	if( info == NULL )
-	{
-		jpa_ThrowError( env, "Pa_GetDeviceInfo returned NULL." );
-	}
-	else
-	{
-		jpa_SetStringField( env, cls, deviceInfo, "name", info->name );
-		jpa_SetIntField( env, cls, deviceInfo, "maxInputChannels", info->maxInputChannels );
-		jpa_SetIntField( env, cls, deviceInfo, "maxOutputChannels", info->maxOutputChannels );
-		jpa_SetIntField( env, cls, deviceInfo, "hostApi", info->hostApi );
-		jpa_SetDoubleField( env, cls, deviceInfo, "defaultSampleRate", info->defaultSampleRate );
-		jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowInputLatency", info->defaultLowInputLatency );
-		jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowInputLatency", info->defaultHighInputLatency );
-		jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowOutputLatency", info->defaultLowOutputLatency );
-		jpa_SetDoubleField( env, cls, deviceInfo, "defaultHighOutputLatency", info->defaultHighOutputLatency );
-	}
+    const PaDeviceInfo *info;
+         /* Get a reference to obj's class */
+    jclass cls = (*env)->GetObjectClass(env, deviceInfo);
+
+    info = Pa_GetDeviceInfo( index );
+    if( info == NULL )
+    {
+        jpa_ThrowError( env, "Pa_GetDeviceInfo returned NULL." );
+    }
+    else
+    {
+        jpa_SetStringField( env, cls, deviceInfo, "name", info->name );
+        jpa_SetIntField( env, cls, deviceInfo, "maxInputChannels", info->maxInputChannels );
+        jpa_SetIntField( env, cls, deviceInfo, "maxOutputChannels", info->maxOutputChannels );
+        jpa_SetIntField( env, cls, deviceInfo, "hostApi", info->hostApi );
+        jpa_SetDoubleField( env, cls, deviceInfo, "defaultSampleRate", info->defaultSampleRate );
+        jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowInputLatency", info->defaultLowInputLatency );
+        jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowInputLatency", info->defaultHighInputLatency );
+        jpa_SetDoubleField( env, cls, deviceInfo, "defaultLowOutputLatency", info->defaultLowOutputLatency );
+        jpa_SetDoubleField( env, cls, deviceInfo, "defaultHighOutputLatency", info->defaultHighOutputLatency );
+    }
 }
 
 /*
@@ -137,8 +137,8 @@ JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_getDeviceInfo
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getHostApiCount
   (JNIEnv *env, jclass clazz)
 {
-	jint count = Pa_GetHostApiCount();
-	return jpa_CheckError( env, count );
+    jint count = Pa_GetHostApiCount();
+    return jpa_CheckError( env, count );
 }
 
 
@@ -150,7 +150,7 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getHostApiCount
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_hostApiTypeIdToHostApiIndex
   (JNIEnv *env, jclass clazz, jint hostApiType)
 {
-	return Pa_HostApiTypeIdToHostApiIndex( (PaHostApiTypeId) hostApiType );
+    return Pa_HostApiTypeIdToHostApiIndex( (PaHostApiTypeId) hostApiType );
 }
 
 /*
@@ -161,7 +161,7 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_hostApiTypeIdToHostApiIndex
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_hostApiDeviceIndexToDeviceIndex
   (JNIEnv *env, jclass clazz, jint hostApiIndex, jint apiDeviceIndex)
 {
-	return Pa_HostApiDeviceIndexToDeviceIndex( hostApiIndex, apiDeviceIndex );
+    return Pa_HostApiDeviceIndexToDeviceIndex( hostApiIndex, apiDeviceIndex );
 }
 
 
@@ -173,24 +173,24 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_hostApiDeviceIndexToDeviceIn
 JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_getHostApiInfo
   (JNIEnv *env, jclass clazz, jint index, jobject hostApiInfo)
 {
-	const PaHostApiInfo *info;
-	     /* Get a reference to obj's class */
-	jclass cls = (*env)->GetObjectClass(env, hostApiInfo);
- 
-	info = Pa_GetHostApiInfo( index );
-	if( info == NULL )
-	{
-		jpa_ThrowError( env, "Pa_GetHostApiInfo returned NULL." );
-	}
-	else
-	{
-		jpa_SetIntField( env, cls, hostApiInfo, "version", info->structVersion );
-		jpa_SetIntField( env, cls, hostApiInfo, "type", info->type );
-		jpa_SetStringField( env, cls, hostApiInfo, "name", info->name );
-		jpa_SetIntField( env, cls, hostApiInfo, "deviceCount", info->deviceCount );
-		jpa_SetIntField( env, cls, hostApiInfo, "defaultInputDevice", info->defaultInputDevice );
-		jpa_SetIntField( env, cls, hostApiInfo, "defaultOutputDevice", info->defaultOutputDevice );
-	}
+    const PaHostApiInfo *info;
+         /* Get a reference to obj's class */
+    jclass cls = (*env)->GetObjectClass(env, hostApiInfo);
+
+    info = Pa_GetHostApiInfo( index );
+    if( info == NULL )
+    {
+        jpa_ThrowError( env, "Pa_GetHostApiInfo returned NULL." );
+    }
+    else
+    {
+        jpa_SetIntField( env, cls, hostApiInfo, "version", info->structVersion );
+        jpa_SetIntField( env, cls, hostApiInfo, "type", info->type );
+        jpa_SetStringField( env, cls, hostApiInfo, "name", info->name );
+        jpa_SetIntField( env, cls, hostApiInfo, "deviceCount", info->deviceCount );
+        jpa_SetIntField( env, cls, hostApiInfo, "defaultInputDevice", info->defaultInputDevice );
+        jpa_SetIntField( env, cls, hostApiInfo, "defaultOutputDevice", info->defaultOutputDevice );
+    }
 }
 
 /*
@@ -201,8 +201,8 @@ JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_getHostApiInfo
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultInputDevice
   (JNIEnv *env, jclass clazz)
 {
-	jint deviceId = Pa_GetDefaultInputDevice();
-	return jpa_CheckError( env, deviceId );
+    jint deviceId = Pa_GetDefaultInputDevice();
+    return jpa_CheckError( env, deviceId );
 }
 
 /*
@@ -213,8 +213,8 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultInputDevice
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultOutputDevice
   (JNIEnv *env, jclass clazz)
 {
-	jint deviceId = Pa_GetDefaultOutputDevice();
-	return jpa_CheckError( env, deviceId );
+    jint deviceId = Pa_GetDefaultOutputDevice();
+    return jpa_CheckError( env, deviceId );
 }
 
 /*
@@ -225,8 +225,8 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultOutputDevice
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultHostApi
   (JNIEnv *env, jclass clazz)
 {
-	jint deviceId = Pa_GetDefaultHostApi();
-	return jpa_CheckError( env, deviceId );
+    jint deviceId = Pa_GetDefaultHostApi();
+    return jpa_CheckError( env, deviceId );
 }
 
 /*
@@ -237,13 +237,13 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_getDefaultHostApi
 JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_isFormatSupported
   (JNIEnv *env, jclass clazz, jobject inParams, jobject outParams, jint sampleRate )
 {
-	PaStreamParameters myInParams, *paInParams;
-	PaStreamParameters myOutParams, *paOutParams;
-	
-	paInParams = jpa_FillStreamParameters(  env, inParams, &myInParams );
-	paOutParams = jpa_FillStreamParameters(  env, outParams, &myOutParams );
-	
-	return Pa_IsFormatSupported( paInParams, paOutParams, sampleRate );
+    PaStreamParameters myInParams, *paInParams;
+    PaStreamParameters myOutParams, *paOutParams;
+
+    paInParams = jpa_FillStreamParameters(  env, inParams, &myInParams );
+    paOutParams = jpa_FillStreamParameters(  env, outParams, &myOutParams );
+
+    return Pa_IsFormatSupported( paInParams, paOutParams, sampleRate );
 
 }
 
@@ -255,25 +255,25 @@ JNIEXPORT jint JNICALL Java_com_portaudio_PortAudio_isFormatSupported
 JNIEXPORT void JNICALL Java_com_portaudio_PortAudio_openStream
   (JNIEnv *env, jclass clazz, jobject blockingStream,  jobject inParams, jobject outParams, jint sampleRate, jint framesPerBuffer, jint flags )
 {
-	int err;
-	PaStreamParameters myInParams, *paInParams;
-	PaStreamParameters myOutParams, *paOutParams;
-	PaStream *stream;
-	
-	paInParams = jpa_FillStreamParameters(  env, inParams, &myInParams );
-	paOutParams = jpa_FillStreamParameters(  env, outParams, &myOutParams );
-	err = Pa_OpenStream( &stream, paInParams, paOutParams, sampleRate, framesPerBuffer, flags, NULL, NULL );
-	if( jpa_CheckError( env, err ) == 0 )
-	{
-		jclass cls = (*env)->GetObjectClass(env, blockingStream);
-		jpa_SetLongField( env, cls, blockingStream, "nativeStream", (jlong) stream );
-		if( paInParams != NULL )
-		{
-			jpa_SetIntField( env, cls, blockingStream, "inputFormat", paInParams->sampleFormat );
-		}
-		if( paOutParams != NULL )
-		{
-			jpa_SetIntField( env, cls, blockingStream, "outputFormat", paOutParams->sampleFormat );
-		}
-	}
+    int err;
+    PaStreamParameters myInParams, *paInParams;
+    PaStreamParameters myOutParams, *paOutParams;
+    PaStream *stream;
+
+    paInParams = jpa_FillStreamParameters(  env, inParams, &myInParams );
+    paOutParams = jpa_FillStreamParameters(  env, outParams, &myOutParams );
+    err = Pa_OpenStream( &stream, paInParams, paOutParams, sampleRate, framesPerBuffer, flags, NULL, NULL );
+    if( jpa_CheckError( env, err ) == 0 )
+    {
+        jclass cls = (*env)->GetObjectClass(env, blockingStream);
+        jpa_SetLongField( env, cls, blockingStream, "nativeStream", (jlong) stream );
+        if( paInParams != NULL )
+        {
+            jpa_SetIntField( env, cls, blockingStream, "inputFormat", paInParams->sampleFormat );
+        }
+        if( paOutParams != NULL )
+        {
+            jpa_SetIntField( env, cls, blockingStream, "outputFormat", paOutParams->sampleFormat );
+        }
+    }
 }
