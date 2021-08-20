4d452f46bfb081760f060abea1769613f961bedd
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2021-08-20 15:16:14 +0300

    PulseAudio: Fix most of the cmakelint warnings

--- cmake/modules/FindPulseAudio.cmake
+++ cmake/modules/FindPulseAudio.cmake
@@ -2,7 +2,8 @@
 # FindPulseAudio
 # --------------
 #
-# This is base on https://raw.githubusercontent.com/KDE/extra-cmake-modules/master/find-modules/FindPulseAudio.cmake
+# This is base on
+# https://raw.githubusercontent.com/KDE/extra-cmake-modules/master/find-modules/FindPulseAudio.cmake
 #
 # Try to locate the PulseAudio library.
 # If found, this will define the following variables:
@@ -54,39 +55,39 @@ pkg_check_modules(PC_PulseAudio QUIET libpulse>=${PulseAudio_FIND_VERSION})
 pkg_check_modules(PC_PulseAudio_MAINLOOP QUIET libpulse-mainloop-glib)
 
 find_path(PulseAudio_INCLUDE_DIRS pulse/pulseaudio.h
-   HINTS
-   ${PC_PulseAudio_INCLUDEDIR}
-   ${PC_PulseAudio_INCLUDE_DIRS}
-   )
+  HINTS
+  ${PC_PulseAudio_INCLUDEDIR}
+  ${PC_PulseAudio_INCLUDE_DIRS}
+  )
 
 find_library(PulseAudio_LIBRARIES NAMES pulse libpulse
-   HINTS
-   ${PC_PulseAudio_LIBDIR}
-   ${PC_PulseAudio_LIBRARY_DIRS}
-   )
+  HINTS
+  ${PC_PulseAudio_LIBDIR}
+  ${PC_PulseAudio_LIBRARY_DIRS}
+  )
 
 find_library(PulseAudio_MAINLOOP_LIBRARY NAMES pulse-mainloop pulse-mainloop-glib libpulse-mainloop-glib
-   HINTS
-   ${PC_PulseAudio_LIBDIR}
-   ${PC_PulseAudio_LIBRARY_DIRS}
-   )
+  HINTS
+  ${PC_PulseAudio_LIBDIR}
+  ${PC_PulseAudio_LIBRARY_DIRS}
+  )
 
 # Store the version number in the cache, so we don't have to search every time again:
-if (PulseAudio_INCLUDE_DIRS AND NOT PulseAudio_VERSION)
+if(PulseAudio_INCLUDE_DIRS AND NOT PulseAudio_VERSION)
 
-   # get PulseAudio's version from its version.h
-   file(STRINGS "${PulseAudio_INCLUDE_DIRS}/pulse/version.h" pulse_version_h
-        REGEX ".*pa_get_headers_version\\(\\).*")
-   string(REGEX REPLACE ".*pa_get_headers_version\\(\\)\ \\(\"([0-9]+\\.[0-9]+\\.[0-9]+)[^\"]*\"\\).*" "\\1"
-                         _PulseAudio_VERSION "${pulse_version_h}")
+  # get PulseAudio's version from its version.h
+  file(STRINGS "${PulseAudio_INCLUDE_DIRS}/pulse/version.h" pulse_version_h
+    REGEX ".*pa_get_headers_version\\(\\).*")
+  string(REGEX REPLACE ".*pa_get_headers_version\\(\\)\ \\(\"([0-9]+\\.[0-9]+\\.[0-9]+)[^\"]*\"\\).*" "\\1"
+    _PulseAudio_VERSION "${pulse_version_h}")
 
-   set(PulseAudio_VERSION "${_PulseAudio_VERSION}" CACHE STRING "Version number of PulseAudio" FORCE)
+  set(PulseAudio_VERSION "${_PulseAudio_VERSION}" CACHE STRING "Version number of PulseAudio" FORCE)
 endif()
 
 # Use the new extended syntax of find_package_handle_standard_args(), which also handles version checking:
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PulseAudio REQUIRED_VARS PulseAudio_LIBRARIES PulseAudio_INCLUDE_DIRS
-                                             VERSION_VAR PulseAudio_VERSION)
+  VERSION_VAR PulseAudio_VERSION)
 
 # Deprecated synonyms
 set(PULSEAUDIO_INCLUDE_DIR "${PulseAudio_INCLUDE_DIRS}")
@@ -97,13 +98,13 @@ set(PULSEAUDIO_FOUND "${PulseAudio_FOUND}")
 if(PulseAudio_FOUND AND NOT TARGET PulseAudio::PulseAudio)
   add_library(PulseAudio::PulseAudio UNKNOWN IMPORTED)
   set_target_properties(PulseAudio::PulseAudio PROPERTIES
-                        IMPORTED_LOCATION "${PulseAudio_LIBRARIES}"
-			INTERFACE_INCLUDE_DIRECTORIES "${PulseAudio_INCLUDE_DIRS}")
+    IMPORTED_LOCATION "${PulseAudio_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${PulseAudio_INCLUDE_DIRS}")
 endif()
 
 mark_as_advanced(PulseAudio_INCLUDE_DIRS PULSEAUDIO_INCLUDE_DIR
-                 PulseAudio_LIBRARIES PULSEAUDIO_LIBRARY
-		 PulseAudio_MAINLOOP_LIBRARY PULSEAUDIO_MAINLOOP_LIBRARY)
+  PulseAudio_LIBRARIES PULSEAUDIO_LIBRARY
+  PulseAudio_MAINLOOP_LIBRARY PULSEAUDIO_MAINLOOP_LIBRARY)
 
 include(FeatureSummary)
 set_package_properties(PulseAudio PROPERTIES
