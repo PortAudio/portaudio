bf5d562f6e35d2aa2d264e92f90666484212a88e
invertego <invertego@users.noreply.github.com>
2023-10-02 16:44:48 -0700

    wdmks: declare GUIDs with selectany attribute
    Match the behavior of guiddef.h in both mingw and the Windows SDK
    headers. This prevents linking errors caused by multiply defined symbols
    when linking against certain Windows SDK libs (like dxguid.lib).

--- src/hostapi/wdmks/pa_win_wdmks.c
+++ src/hostapi/wdmks/pa_win_wdmks.c
@@ -165,11 +165,11 @@ Default is to use the pin category.
 #define _NTRTL_ /* Turn off default definition of DEFINE_GUIDEX */
 #undef DEFINE_GUID
 #if defined(__clang__) || (defined(_MSVC_TRADITIONAL) && !_MSVC_TRADITIONAL) /* clang-cl and new msvc preprocessor: avoid too many arguments error */
-    #define DEFINE_GUID(n, ...) EXTERN_C const GUID n = {__VA_ARGS__}
+    #define DEFINE_GUID(n, ...) EXTERN_C const GUID DECLSPEC_SELECTANY n = {__VA_ARGS__}
     #define DEFINE_GUID_THUNK(n, ...) DEFINE_GUID(n, __VA_ARGS__)
     #define DEFINE_GUIDEX(n) DEFINE_GUID_THUNK(n, STATIC_##n)
 #else
-    #define DEFINE_GUID(n, data) EXTERN_C const GUID n = {data}
+    #define DEFINE_GUID(n, data) EXTERN_C const GUID DECLSPEC_SELECTANY n = {data}
     #define DEFINE_GUID_THUNK(n, data) DEFINE_GUID(n, data)
     #define DEFINE_GUIDEX(n) DEFINE_GUID_THUNK(n, STATIC_##n)
 #endif /* __clang__, !_MSVC_TRADITIONAL */
